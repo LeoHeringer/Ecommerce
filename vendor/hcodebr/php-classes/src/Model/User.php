b9a697b3384600e78796b678ed4c4e360153d0dc
LeoHeringer <leonardoheringer2016@hotmail.com>
2022-10-13 10:13:12 -0300

    carrinho de compras

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -1,4 +1,5 @@
 <?php 
+
 namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
@@ -10,54 +11,120 @@ class User extends Model {
 	const SESSION = "User";
 	const SECRET = "HcodePhp7_Secret";
 
-	public static function login($login, $password)
+	public static function getFromSession()
 	{
-		$sql = new Sql();
-		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
-			":LOGIN"=>$login
-		)); 
-		if (count($results) === 0)
-		{
-			throw new \Exception("Usuário inexistente ou senha inválida.");
+
+		$user = new User();
+
+		if (isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0) {
+
+			$user->setData($_SESSION[User::SESSION]);
+
 		}
-		$data = $results[0];
-		if (password_verify($password, $data["despassword"]) === true)
-		{
-			$user = new User();
-			$user->setData($data);
-			$_SESSION[User::SESSION] = $user->getValues();
-			return $user;
-		} else {
-			throw new \Exception("Usuário inexistente ou senha inválida.");
-		}
+
+		return $user;
+
 	}
-	public static function verifyLogin($inadmin = true)
+
+	public static function checkLogin($inadmin = true)
 	{
+
 		if (
 			!isset($_SESSION[User::SESSION])
 			||
 			!$_SESSION[User::SESSION]
 			||
 			!(int)$_SESSION[User::SESSION]["iduser"] > 0
-			||
-			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
 		) {
+			//Não está logado
+			return false;
+
+		} else {
+
+			if ($inadmin === true && (bool)$_SESSION[User::SESSION]['inadmin'] === true) {
+
+				return true;
+
+			} else if ($inadmin === false) {
+
+				return true;
+
+			} else {
+
+				return false;
+
+			}
+
+		}
+
+	}
+
+	public static function login($login, $password)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+			":LOGIN"=>$login
+		)); 
+
+		if (count($results) === 0)
+		{
+			throw new \Exception("Usuário inexistente ou senha inválida.");
+		}
+
+		$data = $results[0];
+
+		if (password_verify($password, $data["despassword"]) === true)
+		{
+
+			$user = new User();
+
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+		} else {
+			throw new \Exception("Usuário inexistente ou senha inválida.");
+		}
+
+	}
+
+	public static function verifyLogin($inadmin = true)
+	{
+
+		if (User::checkLogin($inadmin)) {
+
 			header("Location: /admin/login");
 			exit;
+
 		}
+
 	}
+
 	public static function logout()
 	{
+
 		$_SESSION[User::SESSION] = NULL;
+
 	}
+
 	public static function listAll()
 	{
+
 		$sql = new Sql();
+
 		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
 	}
+
 	public function save()
 	{
+
 		$sql = new Sql();
+
 		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
 			":desperson"=>$this->getdesperson(),
 			":deslogin"=>$this->getdeslogin(),
@@ -66,19 +133,29 @@ class User extends Model {
 			":nrphone"=>$this->getnrphone(),
 			":inadmin"=>$this->getinadmin()
 		));
+
 		$this->setData($results[0]);
+
 	}
+
 	public function get($iduser)
 	{
+
 		$sql = new Sql();
+
 		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
 			":iduser"=>$iduser
 		));
+
 		$this->setData($results[0]);
+
 	}
+
 	public function update()
 	{
+
 		$sql = new Sql();
+
 		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
 			":iduser"=>$this->getiduser(),
 			":desperson"=>$this->getdesperson(),
@@ -88,11 +165,16 @@ class User extends Model {
 			":nrphone"=>$this->getnrphone(),
 			":inadmin"=>$this->getinadmin()
 		));
+
 		$this->setData($results[0]);		
+
 	}
+
 	public function delete()
 	{
+
 		$sql = new Sql();
+
 		$sql->query("CALL sp_users_delete(:iduser)", array(
 			":iduser"=>$this->getiduser()
 		));
@@ -116,7 +198,7 @@ class User extends Model {
 		if (count($results) === 0)
 		{
 			throw new \Exception("Não foi possível recuperar a senha.");
-
+			
 		}
 		else
 		{
